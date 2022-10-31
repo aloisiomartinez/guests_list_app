186f8ff07f092f22f6ccec42483c14a74fc23697
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-10-31 18:07:08 -0300

    Tela de Cadastro

--- app/src/main/java/com/devmasterteam/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/devmasterteam/convidados/GuestFormActivity.kt
@@ -2,10 +2,34 @@ package com.devmasterteam.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.devmasterteam.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+
         setContentView(R.layout.activity_guest_form)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/devmasterteam/convidados/GuestFormViewModel.kt
@@ -0,0 +1,8 @@
+package com.devmasterteam.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+
+
+}
