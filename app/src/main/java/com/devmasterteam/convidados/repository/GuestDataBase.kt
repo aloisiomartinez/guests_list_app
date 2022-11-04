9d4bb2d9b240f7d7a9fd9b37d4caae4fc5ea2404
aloisiomartinez <aloisiojacobs36@gmail.com>
2022-11-04 17:16:37 -0300

    DataBase Constants

--- app/src/main/java/com/devmasterteam/convidados/repository/GuestDataBase.kt
+++ app/src/main/java/com/devmasterteam/convidados/repository/GuestDataBase.kt
@@ -3,6 +3,7 @@ package com.devmasterteam.convidados.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.devmasterteam.convidados.constants.DataBaseConstants
 
 class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
 
@@ -13,10 +14,10 @@ class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VE
 
     override fun onCreate(db: SQLiteDatabase) {
         // Criação do banco
-        db.execSQL("create table Guest (" +
-                "id integer primary key autoincrement, " +
-                "name text, " +
-                "presence integer);")
+        db.execSQL("create table" +DataBaseConstants.GUEST.TABLE_NAME+ "(" +
+                DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement, " +
+                DataBaseConstants.GUEST.COLUMNS.NAME + " text, " +
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE + " integer);")
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
